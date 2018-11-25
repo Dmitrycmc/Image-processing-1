7e2fc33ee0239928870a10b2da87b4e7bd80d316
Dmitry Litov <Dmitrycmc@gmail.com>
2018-11-25 14:13:57 +0300

    added

--- app_1/Program.cs
+++ app_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace app_1
 { 
@@ -8,6 +9,31 @@ namespace app_1
         {
             try
 			{
+				Bitmap mask = new Bitmap("beg49d.bmp");
+				Bitmap canny = new Bitmap("beg4.jpg");
+				Bitmap orig = new Bitmap("beg9.bmp");
+				
+				int width = mask.Width;
+				int height = mask.Height;
+
+				Bitmap output1 = new Bitmap(width, height);
+				Bitmap output2 = new Bitmap(width, height);
+
+				for (int i = 0; i < width; i++)
+				{
+					for (int j = 0; j < height; j++)
+					{
+						Color color = mask.GetPixel(i, j);
+						Color color1 = Color.Wheat;
+						color1 = (color.R != 0 ? canny : orig).GetPixel(i, j);
+						output1.SetPixel(i, j, color1);
+						color1 = (color.R == 0 ? canny : orig).GetPixel(i, j);
+						output2.SetPixel(i, j, color1);
+					}
+				}
+				output1.Save("rr.bmp");
+				output2.Save("rr1.bmp");
+
 				//Console.WriteLine(Utils.angle(5, 0));
 				Console.WriteLine(
 					"mirror {x|y}\n" +
@@ -28,7 +54,7 @@ namespace app_1
 				Arguments Args = new Arguments(new string[] { "canny", "2", "100", "30", "beg3.bmp", "beg5.jpg"});
                 //Arguments Args = new Arguments(args);
                 Console.WriteLine(Args);
-                Args.execute();
+                //Args.execute();
                 Console.WriteLine("Success!");
             }
             catch (Exception e)
